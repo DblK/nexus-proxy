ec31eba2ebfa2b3f6acbfc33abda527828709942
Remy Boulanouar <rboulanouar@gmail.com>
2017-04-06 16:56:36 +0200

    Add an error message when forgot environment variable DOMAIN_NAME

--- config.sh
+++ config.sh
@@ -7,4 +7,9 @@ echo Build starting nginx config
 echo replacing ___DOMAIN_NAME___/$DOMAIN_NAME
 sed -i "s/___DOMAIN_NAME___/$DOMAIN_NAME/g" /etc/nginx/nginx.conf
 
+if [ -z "$DOMAIN_NAME" ]; then
+    echo "Need to set DOMAIN_NAME"
+    exit 1
+fi 
+
 nginx -g 'daemon off;'
